db088fbe33b93b31f9b7f17621edd970edc20d72
kuldeep vishwakarma <kv752547@gmail.com>
2020-10-17 22:16:23 +0530

    Updates

---
+++ precache-manifest.fcadd4a92f18afd81c8060a7c019ec85.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea03329a4b79c67fea77c61f8b10dd1",
+    "url": "/flexcode/index.html"
+  },
+  {
+    "revision": "9190b0045dc091208c9f",
+    "url": "/flexcode/static/css/2.974c742c.chunk.css"
+  },
+  {
+    "revision": "e7270b2ce8df0ef156e6",
+    "url": "/flexcode/static/css/main.f5c8d392.chunk.css"
+  },
+  {
+    "revision": "9190b0045dc091208c9f",
+    "url": "/flexcode/static/js/2.548a411a.chunk.js"
+  },
+  {
+    "revision": "54e8c4f782a1df2882087d61fdec9005",
+    "url": "/flexcode/static/js/2.548a411a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7270b2ce8df0ef156e6",
+    "url": "/flexcode/static/js/main.7bafe5ff.chunk.js"
+  },
+  {
+    "revision": "8fff0bd94bea07316246",
+    "url": "/flexcode/static/js/runtime-main.72a28a9d.js"
+  },
+  {
+    "revision": "79fc221efc93a1f89d04d70e7fcf2fca",
+    "url": "/flexcode/static/media/25528.79fc221e.jpg"
+  },
+  {
+    "revision": "3125ca4da32cbf530e0bd827ed892c38",
+    "url": "/flexcode/static/media/card.3125ca4d.PNG"
+  },
+  {
+    "revision": "7b66edf5af1c0371f9df186fc42ecf6f",
+    "url": "/flexcode/static/media/exp.7b66edf5.jpg"
+  },
+  {
+    "revision": "2d8f894058d39f2ffa96f16ef0f2a65a",
+    "url": "/flexcode/static/media/home.2d8f8940.png"
+  },
+  {
+    "revision": "88207322bf50c45334faa5300065a87c",
+    "url": "/flexcode/static/media/page.88207322.png"
+  },
+  {
+    "revision": "db948dda1e20b197b7ab68f5fab04795",
+    "url": "/flexcode/static/media/rocket2.db948dda.png"
+  },
+  {
+    "revision": "45a21ae25501192edaf5b7fd1db0a06f",
+    "url": "/flexcode/static/media/sendsome.45a21ae2.PNG"
+  },
+  {
+    "revision": "1cdb42d120fa250b12f4e39d4cf989d4",
+    "url": "/flexcode/static/media/table.1cdb42d1.PNG"
+  },
+  {
+    "revision": "e6e162c8779a1591fccacef4b7ad21ec",
+    "url": "/flexcode/static/media/thumnail.e6e162c8.PNG"
+  },
+  {
+    "revision": "519287f5452271459c928c357e1fc087",
+    "url": "/flexcode/static/media/website.519287f5.PNG"
+  }
+]);
